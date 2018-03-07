f1c8831d23d5d7d03711eb5a8163188cdad24f0e
seb <sebastien@affinitic.be>
2018-03-07 16:08:23 +0100

    Initial commit

---
+++ src/collective/registration/testing.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from plone.app.contenttypes.testing import PLONE_APP_CONTENTTYPES_FIXTURE
+from plone.app.robotframework.testing import REMOTE_LIBRARY_BUNDLE_FIXTURE
+from plone.app.testing import applyProfile
+from plone.app.testing import FunctionalTesting
+from plone.app.testing import IntegrationTesting
+from plone.app.testing import PloneSandboxLayer
+from plone.testing import z2
+
+import collective.registration
+
+
+class CollectiveRegistrationLayer(PloneSandboxLayer):
+
+    defaultBases = (PLONE_APP_CONTENTTYPES_FIXTURE,)
+
+    def setUpZope(self, app, configurationContext):
+        # Load any other ZCML that is required for your tests.
+        # The z3c.autoinclude feature is disabled in the Plone fixture base
+        # layer.
+        self.loadZCML(package=collective.registration)
+
+    def setUpPloneSite(self, portal):
+        applyProfile(portal, 'collective.registration:default')
+
+
+COLLECTIVE_REGISTRATION_FIXTURE = CollectiveRegistrationLayer()
+
+
+COLLECTIVE_REGISTRATION_INTEGRATION_TESTING = IntegrationTesting(
+    bases=(COLLECTIVE_REGISTRATION_FIXTURE,),
+    name='CollectiveRegistrationLayer:IntegrationTesting'
+)
+
+
+COLLECTIVE_REGISTRATION_FUNCTIONAL_TESTING = FunctionalTesting(
+    bases=(COLLECTIVE_REGISTRATION_FIXTURE,),
+    name='CollectiveRegistrationLayer:FunctionalTesting'
+)
+
+
+COLLECTIVE_REGISTRATION_ACCEPTANCE_TESTING = FunctionalTesting(
+    bases=(
+        COLLECTIVE_REGISTRATION_FIXTURE,
+        REMOTE_LIBRARY_BUNDLE_FIXTURE,
+        z2.ZSERVER_FIXTURE
+    ),
+    name='CollectiveRegistrationLayer:AcceptanceTesting'
+)
---
+++ src/collective/registration/tests/test_setup.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""Setup tests for this package."""
+from plone import api
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from collective.registration.testing import COLLECTIVE_REGISTRATION_INTEGRATION_TESTING  # noqa
+
+import unittest
+
+
+class TestSetup(unittest.TestCase):
+    """Test that collective.registration is properly installed."""
+
+    layer = COLLECTIVE_REGISTRATION_INTEGRATION_TESTING
+
+    def setUp(self):
+        """Custom shared utility setup for tests."""
+        self.portal = self.layer['portal']
+        self.installer = api.portal.get_tool('portal_quickinstaller')
+
+    def test_product_installed(self):
+        """Test if collective.registration is installed."""
+        self.assertTrue(self.installer.isProductInstalled(
+            'collective.registration'))
+
+    def test_browserlayer(self):
+        """Test that ICollectiveRegistrationLayer is registered."""
+        from collective.registration.interfaces import (
+            ICollectiveRegistrationLayer)
+        from plone.browserlayer import utils
+        self.assertIn(
+            ICollectiveRegistrationLayer,
+            utils.registered_layers())
+
+
+class TestUninstall(unittest.TestCase):
+
+    layer = COLLECTIVE_REGISTRATION_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.installer = api.portal.get_tool('portal_quickinstaller')
+        roles_before = api.user.get(userid=TEST_USER_ID).getRoles()
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.installer.uninstallProducts(['collective.registration'])
+        setRoles(self.portal, TEST_USER_ID, roles_before)
+
+    def test_product_uninstalled(self):
+        """Test if collective.registration is cleanly uninstalled."""
+        self.assertFalse(self.installer.isProductInstalled(
+            'collective.registration'))
+
+    def test_browserlayer_removed(self):
+        """Test that ICollectiveRegistrationLayer is removed."""
+        from collective.registration.interfaces import \
+            ICollectiveRegistrationLayer
+        from plone.browserlayer import utils
+        self.assertNotIn(
+           ICollectiveRegistrationLayer,
+           utils.registered_layers())
