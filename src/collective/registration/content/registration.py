78e7cc6d7a6bf4e2d1a313ed9a53ae1fb65ec39c
Sébastien Sirtoli <sebastien@affinitic.be>
2018-03-26 10:26:48 +0200

    WIP add subscriber + set default view + set restriction

--- src/collective/registration/content/registration.py
+++ src/collective/registration/content/registration.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
+from Products.CMFPlone.interfaces.constrains import ISelectableConstrainTypes
+from plone.dexterity.content import Container
 from plone.supermodel import model
 from zope.interface import implementer
-from plone.dexterity.content import Container
 
 
 class IRegistration(model.Schema):
@@ -12,3 +13,16 @@ class IRegistration(model.Schema):
 class Registration(Container):
     """
     """
+
+
+def create_registration_event(object, event):
+    object.REQUEST.RESPONSE.redirect(object.absolute_url() + '/++add++Event?')
+
+
+def create_event_event(object, event):
+    if IRegistration.providedBy(object.aq_parent):
+        parent = object.aq_parent
+        parent.manage_addProperty('default_page', object.id, 'string')
+        behavior = ISelectableConstrainTypes(parent)
+        behavior.setConstrainTypesMode(1)
+        behavior.setImmediatelyAddableTypes(('period',))
